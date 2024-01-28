6e18165a5f19b42970393e96ec41d85f6569ab8e
arcrofts2000 <arcrofts2000@gmail.com>
2024-01-28 23:21:57 +0000

    Initial Base.

---
+++ Source/SurvHorr/SurvHorr.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SurvHorr.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SurvHorr, "SurvHorr" );
---
+++ Source/SurvHorr/SurvHorrGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SurvHorrGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SURVHORR_API ASurvHorrGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
